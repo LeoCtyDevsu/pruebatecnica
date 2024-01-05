fde91bf02ad03cd76d25a1c996f7177e68e0ab77
Leandro Carmenaty <lcarmena@pichincha.com>
2024-01-05 12:48:40 -0500

    feat: componente dashboard

---
+++ src/app/interfaces/persona.interfaces.ts
@@ -0,0 +1,6 @@
+export interface IPersona {
+    Cedula: string;
+    Correo: string;
+    Nombre: string;
+    Tribu: string;
+}
--- src/app/pages/dashboard/dashboard.component.ts
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { IPersona } from 'src/app/interfaces/persona.interfaces';
+import * as XLSX from 'xlsx';
 
 @Component({
   selector: 'app-dashboard',
@@ -6,5 +8,25 @@ import { Component } from '@angular/core';
   styleUrls: ['./dashboard.component.scss']
 })
 export class DashboardComponent {
+  rows: IPersona[] = [];
+  sheet: XLSX.WorkSheet | undefined = undefined;
+  chooseFileLabel: string = 'Seleccione un archivo';
 
+  onFileChange(event: any) {
+    const target: DataTransfer = <DataTransfer>(event.target);
+    this.chooseFileLabel = target.files[0].name;
+    const reader: FileReader = new FileReader();
+    reader.readAsBinaryString(target.files[0]);
+    reader.onload = (e: any) => {
+      const binarystr: string = e.target.result;
+      const wb: XLSX.WorkBook = XLSX.read(binarystr, { type: 'binary' });
+      const wsname: string = wb.SheetNames[0];
+      this.sheet = wb.Sheets[wsname];
+      this.rows = XLSX.utils.sheet_to_json<IPersona>(this.sheet);
+    };
+  }
+
+  onDeleteRow(index: number) {
+    this.rows.splice(index, 1);
+  }
 }
